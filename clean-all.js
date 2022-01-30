d4be7519344cf4d4223f47bc3f2cafb84ce6d3dd
Matt | Varghese Mathew <16229800+mattvarghese@users.noreply.github.com>
2022-01-30 12:53:02 -0600

    documentation

--- clean-all.js
+++ clean-all.js
@@ -1,3 +1,9 @@
+// Copyright (C) 2022 Varghese Mathew (Matt)
+// Distributed under GNU GENERAL PUBLIC LICENSE Version 3
+// See ~/license.txt for details
+// Blog: https://mattvarghese-cs.blogspot.com/2022/01/minimal-typescript-react-project.html
+// GitHub: https://github.com/mattvarghese/minimal-typescript-react-template
+
 var console = require('console');
 var exec = require('child_process').exec;
 var os = require('os');
