d4c530e04e641bd1e639476039cc085492e8a900
Matt | Varghese Mathew <16229800+mattvarghese@users.noreply.github>
2022-02-20 17:05:45 -0600

    minor fix

--- clean-all.ts
+++ clean-all.ts
@@ -13,11 +13,11 @@ function puts(_error, stdout, _stderr) {
 
 // Run command depending on the OS
 if (os.type() === 'Linux') {
-    exec("rm -rf build node_modules", puts);
+    exec("rm -rf dist node_modules", puts);
     exec("rm clean-all.js", puts);
 }
 else {
     exec("rmdir /s /q node_modules", puts);
-    exec("rmdir /s /q build", puts);
+    exec("rmdir /s /q dist", puts);
     exec("del clean-all.js", puts);
 }
